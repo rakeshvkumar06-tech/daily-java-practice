14038120e4014c886c7b7e3b4a925b4381ea8a4e
rakeshvkumar06-tech <rakeshvkumar06@gmail.com>
2026-06-10 20:03:40 +0530

    Add files via upload

---
+++ linearsearch.java
@@ -0,0 +1,25 @@
+public class linearsearch {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        int target = 6;
+
+        int index = -1;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                index = i;
+                break;
+            }
+        }
+
+        if (index != -1) {
+            System.out.println("Element found at index: " + index);
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+}
+    
+
